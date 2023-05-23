cf40b1d7c4506f02484721d5c4830f6198715da0
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-05-23 19:16:01 +0200

    Created a small app for URL Shortening using axios for calling backend api of Shrtco.de

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+function UrlShortner() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+  const [loader, setLoader] = useState("false");
+
+  const fetchData = async () => {
+    try {
+      setLoader("true");
+      const res = await axios(`https://api.shrtco.de/v2/shorten?url=${input}`);
+      setLoader("false");
+      setResult(res.data.result.full_short_link);
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const handleClick = () => {
+    setInput("");
+    fetchData();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>URL Shortner</h1>
+      <input
+        type="name"
+        value={input}
+        placeholder="Enter your URL here"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleClick}>Submit</button>
+      {loader === true ? <p>Loading</p> : <p>{result}</p>}
     </div>
   );
 }
 
-export default App;
+export default UrlShortner;
